d5a03cd4c0c2e7d88095058663cf0e1d19927bb3
TomDijkema <tom.dijkema@naturalis.nl>
2023-04-14 10:49:57 +0200

    Add Geological Map to ID Card
    Commit adds a map based on the latitude and longitude of a Specimen to the ID Card on the Search Page (only displayed if coordinates are present).
    
    Adds:
    - Geological Map to Specimen ID Card on Search Page
    
    Bug fixes:
    - Resolve console error about nesting div elements inside tr elements

--- src/components/search/components/IDCard/IDCard.tsx
+++ src/components/search/components/IDCard/IDCard.tsx
@@ -1,6 +1,9 @@
 /* Import Dependencies */
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useMap } from 'react-leaflet';
+import { Icon, LatLngExpression } from 'leaflet';
 import { Row, Col, Card } from 'react-bootstrap';
 
 /* Import Store */
@@ -12,6 +15,9 @@ import { DigitalMedia } from 'global/Types';
 
 /* Import Styles */
 import styles from 'components/search/search.module.scss';
+
+/* Import Sources */
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
 
 /* Import Icons */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -49,6 +55,12 @@ const IDCard = () => {
         } else {
             return '';
         }
+    }
+
+    const ChangeView = ({ center, zoom }: {center: LatLngExpression, zoom: number}) => {
+        const map = useMap();
+        map.setView(center, zoom);
+        return null;
     }
 
     return (
@@ -114,8 +126,37 @@ const IDCard = () => {
             </Row>
             <Row className={styles.IDCardBottom}>
                 <Col className="h-100">
+                    {/*  If present, show Geological Location */}
+                    <Row className={styles.geoLocationBlock}>
+                        <Col>
+                            {(specimen.data['dwc:decimalLatitude'] && specimen.data['dwc:decimalLongitude']) &&
+                                <MapContainer center={[specimen.data['dwc:decimalLatitude'], specimen.data['dwc:decimalLongitude']]}
+                                    zoom={13} scrollWheelZoom={false} style={{ width: "100%", height: "100%" }}
+                                >
+                                    <ChangeView center={[specimen.data['dwc:decimalLatitude'], specimen.data['dwc:decimalLongitude']]}
+                                        zoom={13}
+                                    />
+                                    <TileLayer
+                                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    />
+
+                                    <Marker position={[specimen.data['dwc:decimalLatitude'], specimen.data['dwc:decimalLongitude']]}
+                                        icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}
+                                    >
+                                        <Popup>
+                                            <p className="fw-bold m-0"> Coordinates </p>
+                                            <p className="mt-1 mb-0"> Latitude: {specimen.data['dwc:decimalLatitude']} </p>
+                                            <p className="mt-1"> Longitude: {specimen.data['dwc:decimalLongitude']} </p>
+                                        </Popup>
+                                    </Marker>
+                                </MapContainer>
+                            }
+                        </Col>
+                    </Row>
+
                     {/* If present, show Digital Media */}
-                    <Row className={styles.digitalMediaBlock}>
+                    <Row className={`${styles.digitalMediaBlock} pt-2`}>
                         <Col className="h-100">
                             <div className={`${styles.digitalMediaSlider} h-100 w-auto`}>
                                 {digitalMedia.map((mediaItem) => {

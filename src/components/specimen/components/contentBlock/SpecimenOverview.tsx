92ef8bf555245098eb7f27b1fa5e0ef6c7451f27
TomDijkema <tom.dijkema@naturalis.nl>
2023-05-01 13:46:40 +0200

    Add Organisation Logos
    Commit adds a lot of Organisations Logos by referring to them by url. Specimen Page now fetches these logos from the url instead of the static logos in the webroot folder.
    
    Adds:
    - Source file with Organisation logo media urls (references)
    
    Modifies:
    - Specimen Page to load the media url logos and not from webroot
    
    Removes:
    - Static logos from webroot

--- src/components/specimen/components/contentBlock/SpecimenOverview.tsx
+++ src/components/specimen/components/contentBlock/SpecimenOverview.tsx
@@ -16,6 +16,7 @@ import styles from 'components/specimen/specimen.module.scss';
 
 /* Import Sources */
 import markerIconPng from 'leaflet/dist/images/marker-icon.png';
+import organisationLogosSource from 'sources/organisationLogos.json';
 
 
 /* Props Typing */
@@ -29,15 +30,22 @@ const SpecimenOverview = (props: Props) => {
 
     /* Base variables */
     const specimen = useAppSelector(getSpecimen);
+    const organisationLogos: { [ror: string]: { name: string, logo: string | string[] } } = { ...organisationLogosSource };
 
-    /* Check for Organisation logo */
-    const logo = (ror: string) => {
-        try {
-            return require(`../../../../webroot/img/organisationLogo/${ror}.png`);
-        } catch (err) {
-            return null;
+    /* Check if reference to Organisation logo is present in Source File */
+    let organisationLogoUrl: string = '';
+
+    if (specimen.organisationId) {
+        if (specimen.organisationId.replace('https://ror.org/', '') in organisationLogos) {
+            const logo = organisationLogos[specimen.organisationId.replace('https://ror.org/', '')].logo;
+
+            if (Array.isArray(logo)) {
+                organisationLogoUrl = logo[0];
+            } else {
+                organisationLogoUrl = logo;
+            }
         }
-    };
+    }
 
     return (
         <Row className="h-100 overflow-scroll">
@@ -162,8 +170,8 @@ const SpecimenOverview = (props: Props) => {
                                         </table>
 
                                         <div className="position-absolute d-flex justify-content-end bottom-0 z-0">
-                                            {specimen.data['ods:organisationId'] &&
-                                                <img src={logo(specimen.data['ods:organisationId'].replace('https://ror.org/', ''))}
+                                            {organisationLogoUrl &&
+                                                <img src={organisationLogoUrl}
                                                     className={styles.organisationLogo}
                                                 />
                                             }

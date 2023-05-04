9fb4497b038055a02f993139c036568356c3c907
TomDijkema <tom.dijkema@naturalis.nl>
2023-05-04 10:02:44 +0200

    Add simple test for Advanced Search on Home
    Commit adds simple test for Advanced Search on Home to test coverage report

--- src/tests/components/specimen/Specimen.test.tsx
+++ src/tests/components/specimen/Specimen.test.tsx
@@ -1,6 +1,6 @@
 /* Import Dependencies */
-import { act } from 'react-dom/test-utils';
-import { screen, waitFor } from '@testing-library/react';
+import "@testing-library/react/dont-cleanup-after-each";
+import { screen, waitFor, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import { Routes, Route } from 'react-router-dom';
 
@@ -14,40 +14,36 @@ import SpecimenMock from 'tests/mock/specimen/specimen.json';
 import Specimen from 'components/specimen/Specimen';
 
 
-/* Test if Specimen data is fetched and rendered */
-it('fetches specimen data', async () => {
-    await act(() =>
+describe("Specimen Page Tests", () => {
+    beforeAll(() => {
         renderWithProviders('/ds/20.5000.1025/DW0-BNT-FM0',
             <Routes>
                 <Route path="/ds/:prefix/:suffix"
                     element={<Specimen />}
                 />
             </Routes>
-        )
-    );
+        );
+    });
 
-    expect(screen.getByRole('heading', { name: SpecimenMock.data.attributes.specimenName })).toBeInTheDocument();
-});
+    afterAll(() => {
+        cleanup();
+    });
 
-/* Test if Annotate Modal is toggled when clicked on Specimen property */
-it('toggles annotate modal', async () => {
-    const user = userEvent.setup();
+    /* Test if Specimen data is fetched and rendered */
+    it('fetches specimen data onload', async () => {
+        expect(screen.getByRole('heading', { name: SpecimenMock.data.attributes.specimenName })).toBeInTheDocument();
+    });
 
-    await act(() =>
-        renderWithProviders('/ds/20.5000.1025/DW0-BNT-FM0',
-            <Routes>
-                <Route path="/ds/:prefix/:suffix"
-                    element={<Specimen />}
-                />
-            </Routes>
-        )
-    );
+    /* Test if Annotate Modal is toggled when clicked on Specimen property */
+    it('is able to toggle the annotate modal', async () => {
+        const user = userEvent.setup();
 
-    const modalTrigger = screen.getByRole('modalTrigger').parentElement;
+        const modalTrigger = screen.getByRole('modalTrigger').parentElement;
 
-    if (modalTrigger) {
-        await waitFor(() => user.click(modalTrigger));
-    }
+        if (modalTrigger) {
+            await waitFor(() => user.click(modalTrigger));
+        }
 
-    expect(screen.getByRole('annotateModalCurrentValue')).toBeInTheDocument();
+        expect(screen.getByRole('annotateModalCurrentValue')).toBeInTheDocument();
+    })
 });

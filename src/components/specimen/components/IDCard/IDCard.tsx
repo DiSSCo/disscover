61e3fb0308da858bb8480437812a833414adda50
TomDijkema <tom.dijkema@naturalis.nl>
2023-05-12 14:53:23 +0200

    Add taxonomy fields to Specimen Page
    Commit adds taxonomy fields that are harmonized to the Specimen Page overview.
    
    Adds:
    - Taxonomy fields to Specimen Page overview
    
    Modifies:
    - ID Card's height to be responsive if there is a banner image present or not
    - ID Card properties' height to not be too much, but still has a maximum
    - Original data view table's height to be responsive on present text above or not
    - Source system url is now displayed on top of the original datatable

--- src/components/specimen/components/IDCard/IDCard.tsx
+++ src/components/specimen/components/IDCard/IDCard.tsx
@@ -33,111 +33,115 @@ const IDCard = (props: Props) => {
         <Row className="h-100">
             {/* ID Card Content */}
             <Col className="h-100">
-                <Row className={styles.IDCardBanner}>
-                    <Col className="h-100">
-                        {(specimenDigitalMedia.length > 0) ?
-                            <div className={`${styles.IDCardBannerBackground} h-100 d-flex justify-content-center`}>
-                                <img src={specimenDigitalMedia[0].digitalMediaObject.mediaUrl} alt="banner"
-                                    className="h-100 position-relative rounded-c"
-                                />
-                            </div>
-                            : <h4 className="text-white py-5 text-center">Placeholder image</h4>
-                        }
-                    </Col>
-                </Row>
-                <Row className={`${styles.IDCard} pt-2`}>
-                    <Col className="h-100">
-                        <Card className="h-100">
-                            <Card.Body className="h-100">
-                                <Card.Subtitle className={`${styles.IDCardIdentifier} mb-2 text-muted`}>
-                                    <Row>
-                                        <Col className={`${styles.IDCardTitle} fw-lightBold`}>
-                                            <p> ID Card </p>
-                                        </Col>
-                                        <Col className={`${styles.IDCardTitle} col-md-auto fw-lightBold`}>
-                                            <p> {specimen.id} </p>
+                <div className="h-100 d-flex flex-column">
+                    {specimenDigitalMedia.length > 0 &&
+                        <Row className={styles.IDCardBanner}>
+                            <Col className="h-100">
+                                {(specimenDigitalMedia.length > 0) ?
+                                    <div className={`${styles.IDCardBannerBackground} h-100 d-flex justify-content-center`}>
+                                        <img src={specimenDigitalMedia[0].digitalMediaObject.mediaUrl} alt="banner"
+                                            className="h-100 position-relative rounded-c"
+                                        />
+                                    </div>
+                                    : <h4 className="text-white py-5 text-center">Placeholder image</h4>
+                                }
+                            </Col>
+                        </Row>
+                    }
+                    <Row className={`${styles.IDCard} pt-2 flex-grow-1`}>
+                        <Col className="h-100">
+                            <Card className="h-100">
+                                <Card.Body className="h-100">
+                                    <Card.Subtitle className={`${styles.IDCardIdentifier} mb-2 text-muted`}>
+                                        <Row>
+                                            <Col className={`${styles.IDCardTitle} fw-lightBold`}>
+                                                <p> ID Card </p>
+                                            </Col>
+                                            <Col className={`${styles.IDCardTitle} col-md-auto fw-lightBold`}>
+                                                <p> {specimen.id} </p>
+                                            </Col>
+                                        </Row>
+                                    </Card.Subtitle>
+
+                                    <Row className={styles.IDCardProperties}>
+                                        <Col className="col-md-auto h-100">
+                                            <Row className={styles.IDCardPropertyBlock}>
+                                                <Col className={`${classPropertyBlockHover} rounded-c py-1`}
+                                                    onClick={() => ToggleModal('ods:specimenName')}
+                                                >
+                                                    <span className="fw-lightBold m-0 h-50" role="modalTrigger">Name in collection</span>
+                                                    <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.specimenName} </span>
+                                                </Col>
+                                            </Row>
+                                            <Row className={styles.IDCardPropertyBlock}>
+                                                <Col className={`${classPropertyBlockHover} rounded-c py-1`}
+                                                    onClick={() => ToggleModal('ods:organisationId')}
+                                                >
+                                                    <span className="fw-lightBold m-0 h-50">Specimen provider</span>
+                                                    <br /> <span className={`${styles.IDCardValue} m-0 h-50`}>
+                                                        {specimen.data['ods:organisationName'] ?
+                                                            <a href={specimen.organisationId}
+                                                                target="_blank" rel="noreferrer"
+                                                                className="c-accent h-underline"
+                                                            >
+                                                                {specimen.data['ods:organisationName']}
+                                                            </a>
+                                                            : specimen.organisationId
+                                                        }
+                                                    </span>
+                                                </Col>
+                                            </Row>
+                                            <Row className={styles.IDCardPropertyBlock}>
+                                                <Col className="m-0 py-1">
+                                                    <span className="fw-lightBold m-0 h-50">Object Type</span>
+                                                    <br /> <span className={`${styles.IDCardValue} m-0 h-50`}>Coming Soon!</span>
+                                                </Col>
+                                            </Row>
+                                            <Row className={styles.IDCardPropertyBlock}>
+                                                <Col className={`${classPropertyBlockHover} rounded-c py-1`}
+                                                    onClick={() => ToggleModal('ods:physicalSpecimenId')}
+                                                >
+                                                    <span className="fw-lightBold m-0 h-50">
+                                                        Physical specimen ID ({specimen.physicalSpecimenIdType}):
+                                                    </span>
+                                                    <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.physicalSpecimenId} </span>
+                                                </Col>
+                                            </Row>
+                                            <Row className={styles.IDCardPropertyBlock}>
+                                                <Col className="m-0 py-1">
+                                                    <span className="fw-lightBold m-0 h-50">Specimen Type</span>
+                                                    <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.type} </span>
+                                                </Col>
+                                            </Row>
+                                            <Row className={styles.IDCardPropertyBlock}>
+                                                <Col className={`${classPropertyBlockHover} rounded-c py-1`}
+                                                    onClick={() => ToggleModal('ods:physicalSpecimenCollection')}
+                                                >
+                                                    <span className="fw-lightBold m-0 h-50">In collection</span>
+                                                    <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.physicalSpecimenCollection} </span>
+                                                </Col>
+                                            </Row>
+                                            <Row className={styles.IDCardPropertyBlock}>
+                                                <Col className={`${classPropertyBlockHover} rounded-c py-1 text-truncate`}
+                                                    onClick={() => ToggleModal('dcterms:license')}
+                                                >
+                                                    <span className="fw-lightBold m-0 h-50">License</span>
+                                                    <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.data['dcterms:license']} </span>
+                                                </Col>
+                                            </Row>
+                                            <Row className={styles.IDCardPropertyBlock}>
+                                                <Col className="m-0 py-1">
+                                                    <span className="fw-lightBold m-0 h-50">Source System</span>
+                                                    <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> Coming Soon! </span>
+                                                </Col>
+                                            </Row>
                                         </Col>
                                     </Row>
-                                </Card.Subtitle>
-
-                                <Row className={styles.IDCardProperties}>
-                                    <Col className="col-md-auto h-100">
-                                        <Row className={styles.IDCardPropertyBlock}>
-                                            <Col className={`${classPropertyBlockHover} rounded-c py-1`}
-                                                onClick={() => ToggleModal('ods:specimenName')}
-                                            >
-                                                <span className="fw-lightBold m-0 h-50">Name in collection</span>
-                                                <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.specimenName} </span>
-                                            </Col>
-                                        </Row>
-                                        <Row className={styles.IDCardPropertyBlock}>
-                                            <Col className={`${classPropertyBlockHover} rounded-c py-1`}
-                                                onClick={() => ToggleModal('ods:organisationId')}
-                                            >
-                                                <span className="fw-lightBold m-0 h-50">Specimen provider</span>
-                                                <br /> <span className={`${styles.IDCardValue} m-0 h-50`}>
-                                                    {specimen.data['ods:organisationName'] ?
-                                                        <a href={specimen.organisationId}
-                                                            target="_blank" rel="noreferrer"
-                                                            className="c-accent h-underline"
-                                                        >
-                                                            {specimen.data['ods:organisationName']}
-                                                        </a>
-                                                        : specimen.organisationId
-                                                    }
-                                                </span>
-                                            </Col>
-                                        </Row>
-                                        <Row className={styles.IDCardPropertyBlock}>
-                                            <Col className="m-0 py-1">
-                                                <span className="fw-lightBold m-0 h-50">Object Type</span>
-                                                <br /> <span className={`${styles.IDCardValue} m-0 h-50`}>Coming Soon!</span>
-                                            </Col>
-                                        </Row>
-                                        <Row className={styles.IDCardPropertyBlock}>
-                                            <Col className={`${classPropertyBlockHover} rounded-c py-1`}
-                                                onClick={() => ToggleModal('ods:physicalSpecimenId')}
-                                            >
-                                                <span className="fw-lightBold m-0 h-50">
-                                                    Physical specimen ID ({specimen.physicalSpecimenIdType}):
-                                                </span>
-                                                <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.physicalSpecimenId} </span>
-                                            </Col>
-                                        </Row>
-                                        <Row className={styles.IDCardPropertyBlock}>
-                                            <Col className="m-0 py-1">
-                                                <span className="fw-lightBold m-0 h-50">Specimen Type</span>
-                                                <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.type} </span>
-                                            </Col>
-                                        </Row>
-                                        <Row className={styles.IDCardPropertyBlock}>
-                                            <Col className={`${classPropertyBlockHover} rounded-c py-1`}
-                                                onClick={() => ToggleModal('ods:physicalSpecimenCollection')}
-                                            >
-                                                <span className="fw-lightBold m-0 h-50">In collection</span>
-                                                <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.physicalSpecimenCollection} </span>
-                                            </Col>
-                                        </Row>
-                                        <Row className={styles.IDCardPropertyBlock}>
-                                            <Col className={`${classPropertyBlockHover} rounded-c py-1 text-truncate`}
-                                                onClick={() => ToggleModal('dcterms:license')}
-                                            >
-                                                <span className="fw-lightBold m-0 h-50">License</span>
-                                                <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> {specimen.data['dcterms:license']} </span>
-                                            </Col>
-                                        </Row>
-                                        <Row className={styles.IDCardPropertyBlock}>
-                                            <Col className="m-0 py-1">
-                                                <span className="fw-lightBold m-0 h-50">Source System</span>
-                                                <br /> <span className={`${styles.IDCardValue} m-0 h-50`}> Coming Soon! </span>
-                                            </Col>
-                                        </Row>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
             </Col>
         </Row>
     );

49c5bd5afc75a737ad247d2ef21eda8d4cadd128
TomDijkema <tom.dijkema@naturalis.nl>
2023-05-17 16:00:23 +0200

    Fix Tests
    Home test was broken due to an additional API call which did not have a mock call yet

--- src/tests/mock/APIMock.ts
+++ src/tests/mock/APIMock.ts
@@ -8,6 +8,7 @@ import SpecimenMock from 'tests/mock/specimen/specimen.json';
 import SpecimenAnnotationsMock from 'tests/mock/specimen/specimenAnnotations.json';
 import SpecimenDigitalMediaMock from 'tests/mock/specimen/specimenDigitalMedia.json';
 import SpecimenVersionsMock from 'tests/mock/specimen/specimenVersions.json';
+import SpecimenDisciplineMock from 'tests/mock/specimen/specimenDiscipline.json';
 import UserMock from 'tests/mock/user/user.json';
 import SourceSystemMock from 'tests/mock/sourceSystem/sourceSystem.json';
 
@@ -25,6 +26,7 @@ const Server = setupServer(
     rest.get('/specimens/20.5000.1025/DW0-BNT-FM0/annotations', (_req, res, ctx) => {
         return res(ctx.json(SpecimenAnnotationsMock));
     }),
+    /* Get Specimen Digital Media */
     rest.get('/specimens/20.5000.1025/DW0-BNT-FM0/digitalmedia', (_req, res, ctx) => {
         return res(ctx.json(SpecimenDigitalMediaMock));
     }),
@@ -32,9 +34,15 @@ const Server = setupServer(
     rest.get('/specimens/20.5000.1025/DW0-BNT-FM0/versions', (_req, res, ctx) => {
         return res(ctx.json(SpecimenVersionsMock));
     }),
+    /* Get Specimen Disciplines */
+    rest.get('/specimens/discipline', (_req, res, ctx) => {
+        return res(ctx.json(SpecimenDisciplineMock));
+    }),
+    /* Get User */
     rest.get('/users/SubjectId', (_req, res, ctx) => {
         return res(ctx.json(UserMock));
     }),
+    /* Get Source System */
     rest.get('https://orchestration.dissco.tech/source-system/sourceSystemId', (_req, res, ctx) => {
         return res(ctx.json(SourceSystemMock));
     })

0f8580118a8dc4b4c87106bcef6e057d5843e3cb
TomDijkema <tom.dijkema@naturalis.nl>
2023-07-18 14:39:23 +0200

    Sonar commit 4
    Fourth commit for resolving Sonar Cloud issues

--- src/global/Utilities.ts
+++ src/global/Utilities.ts
@@ -17,14 +17,14 @@ const DetectMobile = (): boolean => {
     let mobile: boolean = false;
 
     /* Test Browser Agent */
-    if (navigator.userAgent.match(/Android/i) ||
-        navigator.userAgent.match(/webOS/i) ||
-        navigator.userAgent.match(/iPhone/i) ||
-        navigator.userAgent.match(/iPod/i) ||
-        navigator.userAgent.match(/BlackBerry/i) ||
-        navigator.userAgent.match(/Windows Phone/i) ||
-        navigator.userAgent.match(/Opera Mini/i) ||
-        navigator.userAgent.match(/IEMobile/)
+    if (/Android/i.exec(navigator.userAgent) ||
+        /webOS/i.exec(navigator.userAgent) ||
+        /iPhone/i.exec(navigator.userAgent) ||
+        /iPod/i.exec(navigator.userAgent) ||
+        /BlackBerry/i.exec(navigator.userAgent) ||
+        /Windows Phone/i.exec(navigator.userAgent) ||
+        /Opera Mini/i.exec(navigator.userAgent) ||
+        /IEMobile/i.exec(navigator.userAgent)
     ) {
         mobile = true;
     }

269b8b88f2533e0eee5594774b682af0d30df569
TomDijkema <tom.dijkema@naturalis.nl>
2023-10-18 11:34:26 +0200

    Automate Type Generation
    Commit automates the type generation of the data model by including it in the Dockerfile run

--- src/app/GenerateTypes.ts
+++ src/app/GenerateTypes.ts
@@ -6,91 +6,91 @@ import { resolve } from 'path';
 
 /* Assertion */
 const Assertion = async () => {
-    writeFileSync('types/Assertion.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'assertions.json'), {}));
+    writeFileSync('src/app/types/Assertion.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'assertions.json'), {}));
 }
 
 Assertion();
 
 /* Chronometric Age */
 const ChronometricAge = async () => {
-    writeFileSync('types/ChronometricAge.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'chronometric_age.json'), {}));
+    writeFileSync('src/app/types/ChronometricAge.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'chronometric_age.json'), {}));
 }
 
 ChronometricAge();
 
 /* Citation */
 const Citation = async () => {
-    writeFileSync('types/Citation.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'citations.json'), {}));
+    writeFileSync('src/app/types/Citation.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'citations.json'), {}));
 }
 
 Citation();
 
 /* Entity Relationship */
 const EntityRelationship = async () => {
-    writeFileSync('types/EntityRelationship.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'entity_relationships.json'), {}));
+    writeFileSync('src/app/types/EntityRelationship.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'entity_relationships.json'), {}));
 }
 
 EntityRelationship();
 
 /* Identification */
 const Identification = async () => {
-    writeFileSync('types/Identification.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'identifications.json'), {}));
+    writeFileSync('src/app/types/Identification.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'identifications.json'), {}));
 }
 
 Identification();
 
 /* Identifier */
 const Identifier = async () => {
-    writeFileSync('types/Identifier.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'identifiers.json'), {}));
+    writeFileSync('src/app/types/Identifier.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'identifiers.json'), {}));
 }
 
 Identifier();
 
 /* Location */
 const Location = async () => {
-    writeFileSync('types/Location.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'location.json'), {}));
+    writeFileSync('src/app/types/Location.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'location.json'), {}));
 }
 
 Location();
 
 /* Agent */
 const Agent = async () => {
-    writeFileSync('types/Agent.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'agent.json'), {}));
+    writeFileSync('src/app/types/Agent.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'agent.json'), {}));
 }
 
 Agent();
 
 /* Occurrence */
 const Occurrence = async () => {
-    writeFileSync('types/Occurrence.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'occurrences.json'), {}));
+    writeFileSync('src/app/types/Occurrence.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'occurrences.json'), {}));
 }
 
 Occurrence();
 
 /* Event */
 const Event = async () => {
-    writeFileSync('types/Event.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'events.json'), {}));
+    writeFileSync('src/app/types/Event.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'events.json'), {}));
 }
 
 Event();
 
 /* Digital Entity */
 const DigitalEntity = async () => {
-    writeFileSync('types/DigitalEntity.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'digital_entity.json'), {}));
+    writeFileSync('src/app/types/DigitalEntity.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'digital_entity.json'), {}));
 }
 
 DigitalEntity();
 
 /* Material Entity */
 const MaterialEntity = async () => {
-    writeFileSync('types/MaterialEntity.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'material_entity.json'), {}));
+    writeFileSync('src/app/types/MaterialEntity.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'material_entity.json'), {}));
 }
 
 MaterialEntity();
 
 /* Digital Specimen */
 const DigitalSpecimen = async () => {
-    writeFileSync('types/DigitalSpecimen.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'digital_specimen.json'), {}))
+    writeFileSync('src/app/types/DigitalSpecimen.d.ts', await compileFromFile(resolve(__dirname, '../sources/dataModel', 'digital_specimen.json'), {}))
 }
 
 DigitalSpecimen();

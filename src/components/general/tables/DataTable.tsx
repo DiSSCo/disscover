96bac9684656f0db62f9c57a03e0035950700b26
TomDijkema <tom.dijkema@naturalis.nl>
2024-01-10 14:18:22 +0100

    Add datatable hover functionality
    Commit adds datatable hover functionality so that if the mouse hovers over rows, they light up.
    
    Adds:
    - Row hover on datatable

--- src/components/general/tables/DataTable.tsx
+++ src/components/general/tables/DataTable.tsx
@@ -1,4 +1,5 @@
 /* Import Dependencies */
+import { useState } from 'react';
 import '@tanstack/react-table'
 import { flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import classNames from 'classnames';
@@ -35,6 +36,7 @@ const DataTable = (props: Props) => {
     /* Base variables */
     const windowDimensions = useAppSelector(getWindowDimensions);
     const oneRem = 0.85 * windowDimensions.vw / 100;
+    const [hoverRowId, setHoverRowId] = useState<number>(-1);
 
     /* Table initiation */
     const table = useReactTable({
@@ -54,15 +56,16 @@ const DataTable = (props: Props) => {
     /* Function to determine table row class names */
     const RowClassNames = (rowId: string) => {
         return classNames({
-            'bgc-white': true
+            [`${styles.dataTableRow} bgc-white`]: true
         });
     }
-
+ 
     /* Function to determine table column class names */
-    const ColumnClassNames = (columnId: string) => {
+    const ColumnClassNames = (columnId: string, rowIndex: number) => {
         return classNames({
-            'fs-4 px-3 py-2 b-bottom-grey': true,
-            'position-sticky z-1 bgc-white': pinnedColumns?.includes(columnId)
+            [`${styles.dataTableColumn} fs-4 px-3 py-2 b-bottom-grey c-pointer`]: true,
+            'position-sticky z-1 bgc-white': pinnedColumns?.includes(columnId),
+            'bgc-tableHover': hoverRowId === rowIndex
         });
     }
 
@@ -105,15 +108,18 @@ const DataTable = (props: Props) => {
                     })}
                 </thead>
 
-                <tbody className="flex-grow-1 overflow-x-hidden overflow-y-scrol bgc-white">
-                    {table.getRowModel().rows.map(row => {
+                <tbody className="flex-grow-1 overflow-x-hidden overflow-y-scrol bgc-white"
+                    onMouseLeave={() => setHoverRowId(-1)}
+                >
+                    {table.getRowModel().rows.map((row, rowIndex) => {
                         let totalRowWidth: number = 0;
 
                         return (
                             <tr key={row.id}
                                 className={RowClassNames(row.id)}
+                                onMouseEnter={() => setHoverRowId(Number(row.id))}
                             >
-                                {row.getVisibleCells().map((cell, index) => {
+                                {row.getVisibleCells().map((cell, cellIndex) => {
                                     const columnWidth: number = cell.column.columnDef.meta?.widthInRem ?
                                         cell.column.columnDef.meta.widthInRem * oneRem
                                         : cell.column.getSize();
@@ -126,9 +132,9 @@ const DataTable = (props: Props) => {
                                                 width: columnWidth,
                                                 minWidth: columnWidth,
                                                 ...(cell.column.columnDef.meta?.pinned && { left: `${totalRowWidth - columnWidth}px` }),
-                                                ...((pinnedColumns?.includes(cell.column.id) && (index + 1) === pinnedColumns.length) && { 'borderRight': '1px solid #D9D9DF' })
+                                                ...((pinnedColumns?.includes(cell.column.id) && (cellIndex + 1) === pinnedColumns.length) && { 'borderRight': '1px solid #D9D9DF' })
                                             }}
-                                            className={ColumnClassNames(cell.column.id)}
+                                            className={ColumnClassNames(cell.column.id, rowIndex)}
                                         >
                                             {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                         </td>

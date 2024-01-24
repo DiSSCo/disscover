65175804c54e0dafbd036c5169422c042aa5733d
TomDijkema <tom.dijkema@naturalis.nl>
2024-01-24 10:21:12 +0100

    Sonar Commit 7
    Seventh commit for resolving Sonar Cloud issues

--- src/components/general/mediaTypes/MediaRepresentation.tsx
+++ src/components/general/mediaTypes/MediaRepresentation.tsx
@@ -1,4 +1,7 @@
 /* Import Dependencies */
+import classNames from "classnames";
+
+/* Import Icons */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideo, faMusic, faFile } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,21 +15,33 @@ interface Props {
     iconClassName: string,
     accessUri?: string,
     format?: string,
-    clickEvent?: Function
+    ClickEvent?: Function
 };
 
 
 const MediaRepresentation = (props: Props) => {
-    const { mediaType, iconClassName, accessUri, format, clickEvent } = props;
+    const { mediaType, iconClassName, accessUri, format, ClickEvent } = props;
 
     /* Base variables */
     let mediaIcon: JSX.Element | undefined;
 
+    /* Function for when media representation is selected */
+    const OnSelect = () => {
+        if (ClickEvent) {
+            ClickEvent()
+        }
+    }
+
+    /* ClassNames */
+    const classMediaIcon = classNames({
+        'c-pointer': OnSelect
+    });
+
     switch (mediaType) {
         case 'StillImage':
-            mediaIcon = <div className={`h-100 w-100 rounded-c b-none d-flex justify-content-center overflow-hidden ${clickEvent && 'c-pointer'}`}
-                onClick={() => { if (clickEvent) { clickEvent() } }}
-                onKeyDown={() => { if (clickEvent) { clickEvent() } }}
+            mediaIcon = <div className={`${classMediaIcon} h-100 w-100 rounded-c b-none d-flex justify-content-center overflow-hidden`}
+                onClick={() => OnSelect()}
+                onKeyDown={() => OnSelect()}
             >
                 <img src={accessUri}
                     alt={`Broken ${accessUri} link`}
@@ -53,7 +68,7 @@ const MediaRepresentation = (props: Props) => {
             if (format === 'application/json' || format === 'application/ld+json') {
                 mediaIcon = <button type="button"
                     className="h-100 w-100 rounded-c b-none mx-auto d-flex justify-content-around align-items-center"
-                    onClick={() => { if (clickEvent) { clickEvent() } }}
+                    onClick={() => OnSelect()}
                 >
                     <img src={IIIFLogo} alt="IIIF Logo" />
                 </button>;

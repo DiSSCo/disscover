96feaa6d6facd80b7e351ede02118476a56e395b
TomDijkema <tom.dijkema@naturalis.nl>
2024-10-30 13:16:12 +0100

    Build fix

--- src/app/GenerateTypes.ts
+++ src/app/GenerateTypes.ts
@@ -26,7 +26,7 @@ const typesDict = {
     annotationTarget: 'https://schemas.dissco.tech/schemas/fdo-type/annotation/0.3.0/annotation-target.json',
     annotationBody: 'https://schemas.dissco.tech/schemas/fdo-type/annotation/0.3.0/annotation-body.json',
     annotation: 'https://schemas.dissco.tech/schemas/fdo-type/annotation/0.3.0/annotation.json',
-    sourceSystem: 'https://schemas.dissco.tech/schemas/fdo-type/source-system/0.3.0/source-system.json'
+    sourceSystem: 'https://schemas.dissco.tech/schemas/fdo-type/source-system/0.3.0/source-system.json',
     environmentalVariable: 'https://schemas.dissco.tech/schemas/fdo-type/shared-model/0.3.0/environmental-variable.json',
     secretVariable: 'https://schemas.dissco.tech/schemas/fdo-type/shared-model/0.3.0/secret-variable.json',
     machineAnnotationService: 'https://schemas.dissco.tech/schemas/fdo-type/machine-annotation-service/0.3.0/machine-annotation-service.json'

7d743f292fc914cb1bf75184fa9f9954aa74c031
TomDijkema <tom.dijkema@naturalis.nl>
2024-11-08 09:24:47 +0100

    Update DigitalSpecimenOverview.tsx

--- src/components/digitalSpecimen/components/contentBlock/components/DigitalSpecimenOverview.tsx
+++ src/components/digitalSpecimen/components/contentBlock/components/DigitalSpecimenOverview.tsx
@@ -16,15 +16,52 @@ type Props = {
 
 /**
  * Component that renders the digital specimen overview content block on the digital specimen page
+ * @param digitalSpecimen The selected digital specimen
  * @returns JSX Component
  */
 const DigitalSpecimenOverview = (props: Props) => {
     const { digitalSpecimen } = props;
 
+    /* Base variables */
+    const collectors: string[] = [];
+
+    /* Construct collectors array */
+    console.log(digitalSpecimen['ods:hasAgent']);
+    digitalSpecimen['ods:hasAgent']?.filter(agent => agent['schema:roleName'] === 'collector').forEach(agent => {
+        if (agent['schema:name']) {
+            collectors.push(agent['schema:name']);
+        }
+    })
+
     return (
         <div className="h-100">
-            {/* Geological Reference */}
             <Row className="h-50 pb-2">
+                {/* Origin */}
+                <Col lg={{ span: 5 }}>
+                    <Card className="h-100 py-2 px-3">
+                        {/* Title */}
+                        <Row>
+                            <Col>
+                                <p className="tc-accent fw-lightBold">Origin</p>
+                            </Col>
+                        </Row>
+                        {/* Fields */}
+                        <Row>
+                            <Col>
+                                {/* Collectors */}
+                                <Row>
+                                    <Col>
+                                        <p>
+                                            <span className="fw-lightBold">Collectors: </span>
+                                            {collectors.join(' & ')}
+                                        </p>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                {/* Geological Reference */}
                 <Col lg={{ span: 7 }}>
                     <Card className="h-100 py-2 px-3">
                         {/* Title */}
@@ -40,9 +77,6 @@ const DigitalSpecimenOverview = (props: Props) => {
                             </Col>
                         </Row>
                     </Card>
-                </Col>
-                <Col lg={{ span: 5 }}>
-                    <Card className="h-100" />
                 </Col>
             </Row>
             {/* */}

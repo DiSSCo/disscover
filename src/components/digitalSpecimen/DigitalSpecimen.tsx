de3deb5197b16647efef2639c3a5850b1ac531b3
TomDijkema <tom.dijkema@naturalis.nl>
2024-12-06 08:30:36 +0100

    Build fix 2

--- src/components/digitalSpecimen/DigitalSpecimen.tsx
+++ src/components/digitalSpecimen/DigitalSpecimen.tsx
@@ -222,9 +222,7 @@ const DigitalSpecimen = () => {
 
             <DigitalSpecimenTourSteps SetSelectedTabIndex={setSelectedTabIndex} />
             <AnnotateTourSteps SetAnnotationMode={setAnnotationMode} />
-            <MasTourSteps annotationMode={annotationMode}
-                SetAnnotationMode={setAnnotationMode}
-            />
+            <MasTourSteps SetAnnotationMode={setAnnotationMode} />
         </div>
     );
 };

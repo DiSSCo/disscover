2cbe38690dd01e617bc9732768847a0d2831139a
Melanie de Leeuw <melanie.deleeuw@naturalis.nl>
2025-09-17 10:10:44 +0200

    Updated the eslint config to the more current flat config

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default defineConfig([
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            parserOptions: {
+                project: './tsconfig.json',
+                ecmaFeatures: {
+                    jsx: true,
+                }
+            }
+        },
+        rules: {
+            // Need to be fixed and/or evaluated
+            "prefer-const": "off",
+            "no-undef": "off",
+            "no-useless-catch": "off",
+            "@typescript-eslint/no-unused-expressions": "off",
+            "@typescript-eslint/no-unsafe-function-type": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/no-unused-vars": "off"
+        },
+    },
+    {
+        files: ['**/*.jsx'],
+        ...reactHooks.configs['recommended-latest'],
+    },
+    {
+        files: ['**/*.jsx'],
+        ...reactRefresh.configs.vite,
+        rules: {
+            "react-refresh/only-export-components": "warn",
+        },
+    },
+    {
+        ignores: ['dist', 'eslint.config.js', './src/app/types/*'],
+    },
+]);

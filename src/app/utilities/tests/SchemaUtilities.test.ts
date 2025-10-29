d1af007b629a8062bc6dfb3fe15249f1a6770957
Melanie de Leeuw <melanie.deleeuw@naturalis.nl>
2025-10-29 14:11:27 +0100

    feat: rewrote individual tests to use it.each to reduce duplication

--- src/app/utilities/tests/SchemaUtilities.test.ts
+++ src/app/utilities/tests/SchemaUtilities.test.ts
@@ -8,42 +8,23 @@ import { ExtractLastSegmentFromPath } from '../SchemaUtilities';
  * Description of tests to validate the functionality in Schema Utilities
  */
 describe("the ExtractLastSegmentFromPath functionality", () => {
-    let jsonPath: string;
-    let result: string | undefined;
-
-    it('should extract the last human-readable part from a JSONPath string', async () => {
-        jsonPath = '$["ods:hasEvents"][0]["ods:hasLocation"]["ods:hasGeoreference"]';
-
-        result = ExtractLastSegmentFromPath(jsonPath);
-
-        expect(result).toBe('Georeference');
-    });
-    it('should extract the last human-readable part from a JSONPath string and ignore index numbers', async () => {
-        jsonPath = '$["ods:hasIdentifications"][0]["ods:hasTaxonIdentification"][0]';
-
-        result = ExtractLastSegmentFromPath(jsonPath);
-
-        expect(result).toBe('Taxon Identification');
-    });
-    it('should return Digital Specimen if jsonPath is root', async () => {
-        jsonPath = '$';
-
-        result = ExtractLastSegmentFromPath(jsonPath);
-
-        expect(result).toBe('Digital Specimen');
-    });
-    it('should return the last human-readable part of a JSONPath if JSONPath only has one part', async () => {
-        jsonPath = '$["ods:hasEntityRelationships"]';
-
-        result = ExtractLastSegmentFromPath(jsonPath);
-
-        expect(result).toBe('Entity Relationships');
-    });
-    it ('should return undefined if jsonPath is empty', async () => {
-        jsonPath = '';
-
-        result = ExtractLastSegmentFromPath(jsonPath);
-
-        expect(result).toBeUndefined();
+    // Each case for this functionality has a description, a jsonPath and the expected outcome
+    it.each([
+        {
+            description: 'a complex path',
+            jsonPath: '$["ods:hasEvents"][0]["ods:hasLocation"]["ods:hasGeoreference"]',
+            expected: 'Georeference'
+        },
+        {
+            description: 'a path ending in an index',
+            jsonPath: '$["ods:hasIdentifications"][0]["ods:hasTaxonIdentification"][0]',
+            expected: 'Taxon Identification'
+        },
+        { description: 'the root path', jsonPath: '$', expected: 'Digital Specimen' },
+        { description: 'a simple path', jsonPath: '$["ods:hasEntityRelationships"]', expected: 'Entity Relationships' },
+        { description: 'an empty path', jsonPath: '', expected: undefined }
+    ])('should correctly extract segment for $description', ({ jsonPath, expected }) => {
+        const result = ExtractLastSegmentFromPath(jsonPath);
+        expect(result).toBe(expected);
     });
 });

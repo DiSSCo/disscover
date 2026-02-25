dfce55509e911ec6c7ae64fef2133500b7cd6847
Melanie de Leeuw <melanie.deleeuw@naturalis.nl>
2026-02-25 10:25:01 +0100

    Rewrote the virtual collection services to use tanstack/react-query

---
+++ src/services/virtualCollectionService/getVirtualCollectionDetails.ts
@@ -0,0 +1,33 @@
+import apiClient from '../apiClient';
+
+/**
+ * Service that retrieves the virtual collection details through the apiClient
+ * Takes pageSize, pageNumber and virtualCollectionID as a parameter object
+ * @returns An array with all digital specimen in the virtual collection
+ */
+export const getVirtualCollectionDetails = async ({ pageSize, pageNumber, virtualCollectionID }:
+    { pageSize: number, pageNumber?: number, virtualCollectionID: string }) => {
+    try {
+        /* Call service and wait for response */
+        const response = await apiClient.get(`digital-specimen/v1/search?virtualCollectionID=https://hdl.handle.net/${virtualCollectionID}`, {
+            params: {
+                pageSize,
+                pageNumber
+            }
+        });
+
+        /* Throw error if response is not as expected */
+        if(!response.data?.data) {
+            throw new Error('Incorrect response format');
+        }
+
+        /* Return response data */
+        return response.data;
+    } catch (error) {
+        /* If error, throw error with generic error message */
+        console.error('Error fetching virtual collections:', error);
+
+        /* Rethrow error for useQuery */
+        throw error;
+    }
+}

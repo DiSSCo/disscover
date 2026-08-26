797f65e4941be57ba57aee0ee1953f5f620b7d24
Melanie de Leeuw <melanie.deleeuw@naturalis.nl>
2026-08-26 11:17:02 +0200

    Deleted scss file because it is empty and we do not need empty files

--- src/pages/DigitalMediaOverview/DigitalMediaOverview.tsx
+++ src/pages/DigitalMediaOverview/DigitalMediaOverview.tsx
@@ -1,9 +1,6 @@
 /* Import components */
 import { Hero } from 'components/Hero/Hero';
 
-/* Import styling */
-import './DigitalMediaOverview.scss';
-
 export const DigitalMediaOverview = () => {
     return (
         <div className="digital-specimen-page">
